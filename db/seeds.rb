91f5950a36e31ceaf96bb4331cc9da7b6435b794
Wesley <matthew282@msn.com>
2022-11-04 15:02:32 -0700

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Airport.create!(airport_code: "ANC", location: "Anchorage, AK")
+Airport.create!(airport_code: "BJI", location: "Bemidji, MN")
+Airport.create!(airport_code: "DRY ", location: "Derry, ME ")
+Airport.create!(airport_code: "DFW", location: "Dallas/Fort Worth, TX")
+Airport.create!(airport_code: "MSP", location: "Minneapolis, MN")
+Airport.create!(airport_code: "PDX", location: "Portland, OR")
+Airport.create!(airport_code: "PHX", location: "Phoenix, AZ")
+Airport.create!(airport_code: "DEN", location: "Denver, CO")
+Airport.create!(airport_code: "LAX", location: "Los Angeles")
+Airport.create!(airport_code: "HNL", location: "Honolulu, HI ")
