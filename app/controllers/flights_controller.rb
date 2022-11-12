53eee3862bad1d54aef96287dacea96e749fa42a
Wesley <matthew282@msn.com>
2022-11-11 17:44:50 -0700

    add search logic for form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,7 @@
 class FlightsController < ApplicationController
   def index
     @user_options = (Flight.all.map {|f| [f.arrival_airport.airport_code, f.arrival_airport.id]}).uniq(&:first)
-    # @flights = Flight.where(id: params[:id])
+    @flights = Flight.where(arrival_airport_id: params[:id])
   end
 end
 
