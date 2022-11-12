00f7e794e56130b66e8aef85836e575cd4c463d5
Wesley <matthew282@msn.com>
2022-11-11 17:31:12 -0700

    fix bugs in @user_options

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,7 @@
 class FlightsController < ApplicationController
   def index
+    @user_options = (Flight.all.map {|f| [f.arrival_airport.airport_code, f.arrival_airport.id]}).uniq(&:first)
+    # @flights = Flight.where(id: params[:id])
   end
 end
+
