1256e7f2744cb4ec1611f9ed99e4c236d9903d66
Wesley <matthew282@msn.com>
2022-11-14 12:21:20 -0700

    rename @user_options variable, add second search parameter

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,8 @@
 class FlightsController < ApplicationController
   def index
-    @user_options = (Flight.all.map {|f| [f.arrival_airport.airport_code, f.arrival_airport.id]}).uniq(&:first)
-    @flights = Flight.where(arrival_airport_id: params[:id])
+    @arrival_options = (Flight.all.map {|f| [f.arrival_airport.airport_code, f.arrival_airport.id]}).uniq(&:first)
+    @departure_options = (Flight.all.map {|f| [f.departure_airport.airport_code, f.departure_airport.id]}).uniq(&:first)
+    @flights = Flight.where(arrival_airport_id: params[:arrival_airport_id], departure_airport_id: params[:departure_airport_id])
   end
 end
 
