19a19dfd20b73c5b810d9589e9765e0cb818f28e
Wesley <matthew282@msn.com>
2022-11-16 16:16:49 -0700

    cleanup

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,8 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = (Airport.all.map {|a| [a.airport_code, a.id]})
-    @flights = Flight.where(arrival_airport_id: params[:arrival_airport_id], departure_airport_id: params[:departure_airport_id])
+    @flights = Flight.where(arrival_airport_id: params[:arrival_airport_id], 
+                            departure_airport_id: params[:departure_airport_id])
   end
 end
 
