77df3615fe9c79dacebff5585535fa8c0be5175f
Wesley <matthew282@msn.com>
2022-11-18 15:20:15 -0700

    styling on search bars

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,6 @@
 class FlightsController < ApplicationController
   def index
-    @airport_options = (Airport.all.map {|a| [a.airport_code, a.id]})
+    @airport_options = (Airport.all.map {|a| [a.location, a.id]})
     @flights = Flight.where(arrival_airport_id: params[:arrival_airport_id], 
                             departure_airport_id: params[:departure_airport_id])
   end
