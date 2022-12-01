f65cde963b54a82dff8173de3265e86ab31500ec
Wesley <matthew282@msn.com>
2022-11-30 17:52:25 -0700

    add logic for #new / strong params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,11 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
   end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, :ticket_count)
+  end  
 end
