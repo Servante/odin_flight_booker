00feb05833c4f9959920b453de1e3ff0abda052e
Wesley <matthew282@msn.com>
2022-12-17 23:45:43 -0700

    add table info

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,14 @@
+# frozen_string_literal: true
+
+# == Schema Information
+
+# Table name: bookings
+
+# id                   :integer
+# flight_id            :string
+# created_at           :datetime
+# updated_at           :datetime
+
 class Booking < ApplicationRecord
   belongs_to :flight, foreign_key: :flight_id
   has_many :passengers
