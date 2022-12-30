80f24a754cfa1991b050da6059b9780866caddf7
Wesley <matthew282@msn.com>
2022-12-30 15:39:56 -0700

    fix typo

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,10 +1,10 @@
 class FlightsController < ApplicationController
   def index
-    # binding.pry
+    # biding.pry
     @airport_options = (Airport.all.map {|a| [a.location, a.id]})
     @flights = Flight.where(arrival_airport_id: params[:arrival_airport_id], 
                             departure_airport_id: params[:departure_airport_id],
-                            departure_date: params[:departure_time])
+                            departure_date: params[:departure_date])
   end
 end
 
