b80d86f355678a795114c6712f4664a895a44eba
Wesley <matthew282@msn.com>
2022-12-30 14:34:50 -0700

    add departure_date as a search criteria

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,8 +1,10 @@
 class FlightsController < ApplicationController
   def index
+    # binding.pry
     @airport_options = (Airport.all.map {|a| [a.location, a.id]})
     @flights = Flight.where(arrival_airport_id: params[:arrival_airport_id], 
-                            departure_airport_id: params[:departure_airport_id])
+                            departure_airport_id: params[:departure_airport_id],
+                            departure_date: params[:departure_time])
   end
 end
 
