e54b7147a8dda704843257aa55b1a20450d0d651
Wesley <matthew282@msn.com>
2023-01-02 16:35:56 -0700

    fix validations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -15,11 +15,8 @@
 
 
 class Flight < ApplicationRecord
-  #validations
-
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
   has_many :bookings
-  has_many :passengers, through: :bookings
-  
+  has_many :passengers, through: :bookings  
 end
