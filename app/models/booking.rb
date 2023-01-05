d7aa3c4ff1d2309c75cb5932b29c17bf008b3c3e
Wesley <matthew282@msn.com>
2023-01-05 08:49:02 -0700

    remove comment

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -11,6 +11,6 @@
 
 class Booking < ApplicationRecord
   belongs_to :flight, foreign_key: :flight_id
-  has_many :passengers
+  has_many :passengers, dependent: :destroy
   accepts_nested_attributes_for :passengers
 end
