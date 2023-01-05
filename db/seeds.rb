3d923f12a092b8fd484919f611c67d085357f94c
Wesley <matthew282@msn.com>
2023-01-05 10:59:01 -0700

    fix errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+#frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,31 +8,23 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# Airport.destroy_all
-# Flight.destroy_all
+Airport.destroy_all
+Flight.destroy_all
 
+Airport.find_or_create_by(airport_code: "SFO", location: "San Francisco, CA")
+Airport.find_or_create_by(airport_code: "NYC", location: "New York City, NY")
+Airport.find_or_create_by(airport_code: "ATL", location: "Atlanta, GA")
+Airport.find_or_create_by(airport_code: "ORD", location: "Chicago, IL")
+Airport.find_or_create_by(airport_code: "LAX", location: "Los Angeles, CA")
+Airport.find_or_create_by(airport_code: "DFW", location: "Dallas, TX")
+Airport.find_or_create_by(airport_code: "MCO", location: "Orlando, FL")
+Airport.find_or_create_by(airport_code: "DEN", location: "Denver, CO")
 
+start = Time.zone.today
+finish = Time.zone.today + 31.days
 
-# Airport.create!(airport_code: "ANC", location: "Anchorage, AK")
-# Airport.create!(airport_code: "BJI", location: "Bemidji, MN")
-# Airport.create!(airport_code: "DRY ", location: "Derry, ME ")
-# Airport.create!(airport_code: "DFW", location: "Dallas/Fort Worth, TX")
-# Airport.create!(airport_code: "MSP", location: "Minneapolis, MN")
-# Airport.create!(airport_code: "PDX", location: "Portland, OR")
-# Airport.create!(airport_code: "PHX", location: "Phoenix, AZ")
-# Airport.create!(airport_code: "DEN", location: "Denver, CO")
-# Airport.create!(airport_code: "LAX", location: "Los Angeles")
-# Airport.create!(airport_code: "HNL", location: "Honolulu, HI ")
+(start..finish).each do |date|
+  flight_generator = FlightGenerator.new(date)
+  flight_generator.call
+end
 
-
-# Flight.create!(departure_airport_id: 81, arrival_airport_id: 82, start: Time.now, flight_duration: '10hr', )
-# Flight.create!(departure_airport_id: 81, arrival_airport_id: 83, start: Time.now, flight_duration: '9hr')
-# Flight.create!(departure_airport_id: 82, arrival_airport_id: 85, start: Time.now, flight_duration: '8hr')
-# Flight.create!(departure_airport_id: 88, arrival_airport_id: 90, start: Time.now, flight_duration: '7hr')
-# Flight.create!(departure_airport_id: 82, arrival_airport_id: 84, start: Time.now, flight_duration: '6hr')
-# Flight.create!(departure_airport_id: 86, arrival_airport_id: 87, start: Time.now, flight_duration: '5hr')
-# Flight.create!(departure_airport_id: 86, arrival_airport_id: 81, start: Time.now, flight_duration: '4hr')
-# Flight.create!(departure_airport_id: 83, arrival_airport_id: 88, start: Time.now, flight_duration: '3hr')
-# Flight.create!(departure_airport_id: 86, arrival_airport_id: 90, start: Time.now, flight_duration: '2hr')
-# Flight.create!(departure_airport_id: 83, arrival_airport_id: 86, start: Time.now, flight_duration: '1hr')
-# Flight.create!(departure_airport_id: 82, arrival_airport_id: 86, start: Time.now, flight_duration: '1hr')
