3b6c33b5d71361c1d58496402f47675f09cdf9c1
Wesley <matthew282@msn.com>
2023-01-27 12:26:14 -0700

    create passenger_mailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,10 @@
 class PassengerMailer < ApplicationMailer
+  default from: 'wesley.larson73@gmail.com'
+
+  def confirmation_email
+    @booking = Booking.find(params[:booking].id)
+    @url = booking_url(@booking)
+    mail(to: params[:passenger].email,
+          subject: 'You have booked your ticket.')
+  end
 end
