7e7f4aa00c5d41847deb80d5d02f19ddaced2f6b
Wesley <matthew282@msn.com>
2023-04-18 23:40:04 -0700

    clean up comments

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -12,8 +12,6 @@
 
 
 class Airport < ApplicationRecord
-  #validations
-
   has_many :arriving_flights, class_name: 'Flight', foreign_key: :arrival_airport_id
   has_many :departing_flights, class_name: 'Flight', foreign_key: :departure_airport_id
 end
