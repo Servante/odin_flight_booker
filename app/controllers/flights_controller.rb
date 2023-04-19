512b27ab2c51966a2b719529b522b5eabccef8cf
Wesley <matthew282@msn.com>
2023-04-18 19:34:26 -0700

    remove pry

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,5 @@
 class FlightsController < ApplicationController
   def index
-    # biding.pry
     @airport_options = (Airport.all.map {|a| [a.location, a.id]})
     @flights = Flight.where(arrival_airport_id: params[:arrival_airport_id], 
                             departure_airport_id: params[:departure_airport_id],
