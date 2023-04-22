d5e7828df373460aaa3ef0d332b40873a4f339a1
Wesley <matthew282@msn.com>
2023-04-22 15:41:59 -0700

    write feature tests

--- spec/features/searching_flights_spec.rb
+++ spec/features/searching_flights_spec.rb
@@ -6,17 +6,40 @@ RSpec.feature "Users can search flights" do
   let!(:los_angeles) { create(:airport, :los_angeles, location: "Los Angeles, CA") }
   let!(:denver) { create(:airport, :denver, location: "Denver, CO") }
 
-  let (:flight) { create(:flight, id: 1, departure_airport: denver, arrival_airport: los_angeles, departure_date: "2023-04-21") }
+  let! (:NYC_LAX) { create(:flight, id: 6000, departure_airport: denver, arrival_airport: los_angeles) }
 
   scenario "with valid selections" do
     visit "/"
     select("Los Angeles, CA", from: "arrival_airport_id")
     select("Denver, CO", from: "departure_airport_id")
     select(3, from: "ticket_count")
-    fill_in("departure_date", with: "2023-04-21")
+    fill_in("departure_date", with: Time.zone.tomorrow)
     click_on("Search")
     within("#search-results") do
       expect(page).to have_content("LAX")
+    end
+  end
+
+  scenario "with matching invalid arrival and departure airports" do
+    visit "/"
+    select("Los Angeles, CA", from: "arrival_airport_id")
+    select("Los Angeles, CA", from: "departure_airport_id")
+    select(3, from: "ticket_count")
+    fill_in("departure_date", with: Time.zone.tomorrow)
+    click_on("Search")
+    within(".flash-message") do
+      expect(page).to have_content("Please select two different locations!")
+    end
+  end
+
+  xscenario "with no flights available that day" do
+    visit "/"
+    select("Los Angeles, CA", from: "arrival_airport_id")
+    select("Denver, CO", from: "departure_airport_id")
+    fill_in("departure_date", with: Time.zone.today)
+    click_on("Search")
+    within(".flash-message") do
+      expect(page).to have_content("Sorry, there are no available flights for that day.")
     end
   end
 end
