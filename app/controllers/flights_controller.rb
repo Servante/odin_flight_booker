909ba1dac0e4729a9437426cd7e3884c8bbb3bf6
Wesley <matthew282@msn.com>
2023-04-22 17:03:44 -0700

    add strong params

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,9 +1,17 @@
+# frozen_string_literal: true
+
 class FlightsController < ApplicationController
   def index
     @airport_options = (Airport.all.map {|a| [a.location, a.id]})
     @flights = Flight.where(arrival_airport_id: params[:arrival_airport_id], 
                             departure_airport_id: params[:departure_airport_id],
                             departure_date: params[:departure_date])
+  end
+
+  private 
+
+  def search_params
+    params.permit(:arrival_airport_id, :departure_airport_id, :departure_date)
   end
 end
 
