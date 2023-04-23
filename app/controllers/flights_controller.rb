0d26d2bd4e0cd79f8a9242569d0cb0fe5ea0bc1a
Wesley <matthew282@msn.com>
2023-04-22 17:09:14 -0700

    update @flights with strong params

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,9 +3,9 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = (Airport.all.map {|a| [a.location, a.id]})
-    @flights = Flight.where(arrival_airport_id: params[:arrival_airport_id], 
-                            departure_airport_id: params[:departure_airport_id],
-                            departure_date: params[:departure_date])
+    @flights = Flight.where(arrival_airport_id: search_params[:arrival_airport_id], 
+                            departure_airport_id: search_params[:departure_airport_id],
+                            departure_date: search_params[:departure_date])
   end
 
   private 
