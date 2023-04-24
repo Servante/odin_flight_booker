349f770f393e953b18f2a9f5ad2c0d37475e85f5
Wesley <matthew282@msn.com>
2023-04-24 14:27:19 -0700

    reorganize search logic / create find_flights method / add warning for selecting same arrival and departure airports

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,15 +3,27 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = (Airport.all.map {|a| [a.location, a.id]})
-    @flights = Flight.where(arrival_airport_id: search_params[:arrival_airport_id], 
-                            departure_airport_id: search_params[:departure_airport_id],
-                            departure_date: search_params[:departure_date])
+    return if search_params.empty?
+
+
+    @flights = find_flights
   end
 
   private 
 
   def search_params
-    params.permit(:arrival_airport_id, :departure_airport_id, :departure_date)
+    params.permit(:arrival_airport_id, :departure_airport_id, :departure_date, :ticket_count, :commit)
+  end
+
+  def find_flights 
+    if search_params[:arrival_airport_id] === params[:departure_airport_id] 
+      flash.now[:alert] = "Please select two different locations!"
+      render :index
+    else
+      return flights = Flight.where(arrival_airport_id: search_params[:arrival_airport_id], 
+        departure_airport_id: search_params[:departure_airport_id],
+        departure_date: search_params[:departure_date])
+    end
   end
 end
 
