a07c8e6f34ff42f6e9157bd065ed586ef94e0c6a
Wesley <matthew282@msn.com>
2023-04-26 00:48:57 -0700

    update schema commend to reflect new column

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -7,9 +7,11 @@
 # id                   :integer
 # name                 :string
 # email                :string
+# booking_id           :integer
 # created_at           :datetime
 # updated_at           :datetime
 
 class Passenger < ApplicationRecord
   belongs_to :booking
+  has_many :flights, through: :booking
 end
