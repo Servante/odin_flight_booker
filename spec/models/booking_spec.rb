b3e5759a252fdb9371ddf2b41861d6988ed05671
Wesley <matthew282@msn.com>
2023-04-26 13:11:01 -0700

    write test for associations / all tests passing

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Booking, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:booking) { described_class.new }
+
+  describe 'associations' do
+    it { should have_many(:passengers).dependent(:destroy) }
+    it { should belong_to(:flight) }
+    it { should accept_nested_attributes_for(:passengers) }
+  end
 end
