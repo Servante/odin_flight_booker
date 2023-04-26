6c316d747b01daa45353d9d9634c602c348c97be
Wesley <matthew282@msn.com>
2023-04-26 12:54:50 -0700

    add model tests / all passing

--- spec/models/airport_spec.rb
+++ spec/models/airport_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Airport, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:airport) { described_class.new }
+
+  describe 'associations' do
+    it { should have_many(:arriving_flights).dependent(:destroy) }
+    it { should have_many(:departing_flights).dependent(:destroy) }
+  end
 end
