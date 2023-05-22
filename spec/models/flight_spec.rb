75e19dde62b9b5794d41d46d9f83ab4f4785d363
Wesley <matthew282@msn.com>
2023-05-22 11:29:12 -0700

    write test for #time_conversion

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -11,4 +11,17 @@ RSpec.describe Flight, type: :model do
     it { should have_many(:bookings).dependent(:destroy) }
     it { should have_many(:passengers) }
   end
+
+  context 'for methods that return' do
+    let(:atlanta) { create(:airport, :atlanta) }
+    let(:chicago) { create(:airport, :chicago) }
+    subject(:flight_to_ORD) do
+      create (:flight, 
+              departure_id: atlanta.id,
+              arrival_id: chicago.id,
+              flight_duration: 224)
+      end
+
+    describe
+  end
 end
