b29e49459bd7db281191a4b970a2d240f008433b
Wesley <matthew282@msn.com>
2023-06-02 11:50:47 -0700

    add doubles, all tests red

--- spec/mailers/welcome_mailer_spec.rb
+++ spec/mailers/welcome_mailer_spec.rb
@@ -1,8 +1,36 @@
 #frozen_string_literal: true
+require 'rails_helper'
 
 RSpec.describe PassengerMailer, type: :mailer do 
   describe 'welcome_email' do 
-    let (:passenger) { create(:passenger, name: "Vada", email: "vada@example.com") }
-    let(:mail) { welcome_mailer}
+    #create airports
+    let!(:minneapolis) { create(:airport) }
+    let!(:new_york_city) { create(:airport) }
+    #create flight
+    let!(:minneapolis_to_new_york_city) { create(:flight, id: 4200, departure_airport: minneapolis, arrival_airport: new_york_city) }
+    #create booking
+    let! (:vada_booking) { create(:booking, flight_id: 4200) }
+    #create passenger
+    let! (:passenger) { create(:passenger, booking: vada_booking, name: "Vada", email: "vada@example.com") }
+    #create mailer
+    let! (:mail) { PassengerMailer.welcome_email(passenger) }
+
+   
+
+    it 'renders the subject' do
+      expect(mail.subject).to eq('Welcome to Flight Booker')
+    end
+
+    xit 'renders the receiver email' do
+      expect(mail.to).to eq(user.email)
+    end
+
+    xit 'renders the sender email' do
+      expect(mail.from).to eq('weslarsondev@gmail.com')
+    end
+
+    xit 'includes a welcom message in the body' do
+      expect(mail.body).to match ('Welcome to Flight Booker!')
+    end
   end
 end
