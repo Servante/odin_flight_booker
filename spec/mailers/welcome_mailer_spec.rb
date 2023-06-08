543547f2ba6e229b34f8219b3e56dde3352611a8
Wesley <matthew282@msn.com>
2023-06-08 01:09:28 -0700

    all tests passing

--- spec/mailers/welcome_mailer_spec.rb
+++ spec/mailers/welcome_mailer_spec.rb
@@ -9,28 +9,33 @@ RSpec.describe PassengerMailer, type: :mailer do
     #create flight
     let!(:minneapolis_to_new_york_city) { create(:flight, id: 4200, departure_airport: minneapolis, arrival_airport: new_york_city) }
     #create booking
-    let! (:vada_booking) { create(:booking, flight_id: 4200) }
+    let! (:vada_booking) { create(:booking, flight_id: 4200, id: 23) }
     #create passenger
     let! (:passenger) { create(:passenger, booking: vada_booking, name: "Vada", email: "vada@example.com") }
     #create mailer
-    let! (:mail) { PassengerMailer.welcome_email(passenger) }
+    let! (:mail) { PassengerMailer.with(booking: vada_booking, passenger: passenger).confirmation_email }
 
    
 
     it 'renders the subject' do
-      expect(mail.subject).to eq('Welcome to Flight Booker')
+      expect(mail.subject).to eq("You have booked your ticket.")
     end
 
-    xit 'renders the receiver email' do
-      expect(mail.to).to eq(user.email)
+    it 'renders the receiver email' do
+      expect(mail.to).to eq(["#{passenger.email}"])
     end
 
-    xit 'renders the sender email' do
-      expect(mail.from).to eq('weslarsondev@gmail.com')
+    it 'renders the sender email' do
+      expect(mail.from).to eq(["weslarsondev@gmail.com"])
     end
 
-    xit 'includes a welcom message in the body' do
-      expect(mail.body).to match ('Welcome to Flight Booker!')
+    it 'includes a welcome message in the body' do
+      text = "Hello #{passenger.name}, your flight has been booked."
+      expect(mail.body.encoded).to match(text)
+    end
+
+    it 'sends a confirmation email' do 
+      expect {mail.deliver_now}.to change { ActionMailer::Base.deliveries.size }.by(1)
     end
   end
 end
